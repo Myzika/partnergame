782f6f28e0a65992adbf325083437dda4ee1414c
Jordan Wong <ajordaninlimbo@gmail.com>
2018-06-04 01:29:11 -0700

    Finished level 2, added assets for 3

--- game/testing.js
+++ game/testing.js
@@ -2,8 +2,13 @@ function game() {
     const canvas = document.getElementById("myCanvas");
     const ctx = canvas.getContext("2d");
     let hasDoorknob = false;
+    let decision = false;
+    let takeEm = false;
+    let flashlight = true;
+    let keycard = false;
 
-    levelOne();
+    // levelOne();
+    levelTwo();
 
     function levelOne() {
         /**
@@ -39,6 +44,7 @@ function game() {
              * These take the red value of the clicked pixel and execute different displays.
              */
             if (data[0] === 5) {
+                decision = true;
                 ctx.clearRect(70, 966, 1211, 336);
                 ctx.font = "60px VT323";
                 ctx.fillStyle = "white";
@@ -50,6 +56,7 @@ function game() {
                 ctx.drawImage(document.getElementById("no"), 966, 1134);
             }
             else if (data[0] === 80) {
+                decision = false;
                 ctx.clearRect(70, 966, 1211, 336);
                 ctx.font = "60px VT323";
                 ctx.fillStyle = "white";
@@ -57,6 +64,7 @@ function game() {
                 ctx.fillText("Just a door. It's very sturdy...", 672, 1100);
             }
             else if (data[0] === 85) {
+                decision = false;
                 ctx.clearRect(70, 966, 1211, 336);
                 ctx.font = "60px VT323";
                 ctx.fillStyle = "white";
@@ -66,6 +74,7 @@ function game() {
                 ctx.fillText("seem like there were others.", 672, 1240);
             }
             else if (data[0] === 215) {
+                decision = false;
                 if (hasDoorknob === true) {
                     ctx.clearRect(70, 966, 1211, 336);
                     ctx.font = "60px VT323";
@@ -86,7 +95,7 @@ function game() {
                     ctx.fillText("Seems like the doorknob is missing.", 672, 1100);
                 }
             }
-            else if (x > 966) {
+            else if (x > 966 && decision == true) {
                 if (y > 945) {
                     if (y < 1134) {
                         ctx.clearRect(70, 966, 1211, 336);
@@ -106,6 +115,7 @@ function game() {
                         ctx.fillText("You put the jacket back on the hook.", 672, 1100);
                     }
                 }
+
             }
         };
 
@@ -113,5 +123,131 @@ function game() {
 
     function levelTwo() {
         ctx.drawImage(document.getElementById("twobackground"), 0, 0);
+        ctx.drawImage(document.getElementById("twowindows"), 0, 50);
+        ctx.drawImage(document.getElementById("twolights"), 950, 0);
+        ctx.drawImage(document.getElementById("twoblood"), 483, 630);
+        ctx.drawImage(document.getElementById("twofardoor"), 1133, 483);
+
+        canvas.onmousedown = function () {
+            levelTwoGame(event)
+        };
+
+        function levelTwoGame(event) {
+            const rect = canvas.getBoundingClientRect(),
+                x = event.clientX - rect.left,
+                y = event.clientY - rect.top;
+            let data = ctx.getImageData(x, y, 1, 1).data;
+
+            /**
+             * These take the red value of the clicked pixel and execute different displays.
+             */
+            if (data[0] === 93) { //blood
+                takeEm = true;
+                ctx.clearRect(70, 966, 1211, 336);
+                ctx.font = "60px VT323";
+                ctx.fillStyle = "white";
+                ctx.textAlign = "center";
+                ctx.fillText("A dark pool of blood is visible on the floor", 672, 1100);
+                ctx.fillText("It smells terrible and upon closer inspection", 672, 1170);
+                ctx.fillText("there's a keycard and a flashlight in it.", 672, 1240);
+                setTimeout(
+                    function () {
+                        ctx.clearRect(70, 966, 1211, 336);
+                        ctx.fillText("Take them?", 515, 1100);
+                        ctx.drawImage(document.getElementById("yes"), 966, 945);
+                        ctx.drawImage(document.getElementById("no"), 966, 1134);
+                    }, 4 //4000
+                )
+            }
+            else if (data[0] === 16) { //windows
+                takeEm = false;
+                ctx.clearRect(70, 966, 1211, 336);
+                ctx.font = "60px VT323";
+                ctx.fillStyle = "white";
+                ctx.textAlign = "center";
+                ctx.fillText("it's a window", 672, 1100);
+                ctx.fillText("what did you", 672, 1170);
+                ctx.fillText("really expect.", 672, 1240);
+            }
+            else if (data[0] === 82) { //door
+                door();
+            }
+            else if (x > 966 && takeEm === true) {
+                if (y > 945) {
+                    if (y < 1134) {
+                        ctx.clearRect(70, 966, 1211, 336);
+                        ctx.font = "60px VT323";
+                        ctx.fillStyle = "white";
+                        ctx.textAlign = "center";
+                        ctx.fillText("You pick up the keycard and you", 672, 1100);
+                        ctx.fillText("pick up the flashlight. After wiping", 672, 1170);
+                        ctx.fillText("them down, you put them in your pocket.", 672, 1240);
+                        flashlight = true;
+                        keycard = true;
+                    }
+                    else if (y > 1134) {
+                        ctx.clearRect(70, 966, 1211, 336);
+                        ctx.font = "60px VT323";
+                        ctx.fillStyle = "white";
+                        ctx.textAlign = "center";
+                        ctx.fillText("You walk away from the puddle of blood.", 672, 1100);
+                    }
+                }
+
+            }
+        };
+
+        function door() {
+            if (flashlight === true) {
+                ctx.drawImage(document.getElementById("twobgdoor"), 0, 0);
+                ctx.drawImage(document.getElementById("twodoor"), 360, 200);
+                ctx.drawImage(document.getElementById("twogreen"), 1008, 525);
+                ctx.drawImage(document.getElementById("twored"), 1113, 525);
+                ctx.drawImage(document.getElementById("twokeycard"), 1030, 630);
+                canvas.onmousedown = function () {
+                    if (keycard === true) {
+                        ctx.clearRect(70, 966, 1211, 336);
+                        ctx.font = "60px VT323";
+                        ctx.fillStyle = "white";
+                        ctx.textAlign = "center";
+                        ctx.fillText("You slide the slightly bloody keycard", 672, 1100);
+                        ctx.fillText("into the slot, and push the door gently.", 672, 1170);
+                        ctx.fillText("It swings open without hesitation.", 672, 1240);
+                        setTimeout(
+                            levelThree, 4000
+                        )
+                    }
+                    else {
+                        ctx.clearRect(70, 966, 1211, 336);
+                        ctx.font = "60px VT323";
+                        ctx.fillStyle = "white";
+                        ctx.textAlign = "center";
+                        ctx.fillText("The door seems to be locked", 672, 1100);
+                        ctx.fillText("and it seems to require a keycard", 672, 1170);
+                        ctx.fillText("Click to return to last room.", 672, 1240);
+                        canvas.onmousedown = function () {
+                            levelTwo();
+                        }
+                    }
+                }
+            }
+            else {
+                ctx.fillStyle = "#000000";
+                ctx.fillRect(0, 0, 1344, 1344);
+                ctx.clearRect(70, 966, 1211, 336);
+                ctx.font = "60px VT323";
+                ctx.fillStyle = "white";
+                ctx.textAlign = "center";
+                ctx.fillText("I can't see anything!", 672, 1100);
+                canvas.onmousedown = function () {
+                    levelTwo();
+                }
+            }
+
+        }
+    }
+
+    function levelThree() {
+
     }
 }
